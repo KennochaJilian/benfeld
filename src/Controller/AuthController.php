9c7babb5070a38d649bb046da5b6ce35871db083
Aranxa <tassa780@gmail.com>
2021-06-19 20:13:35 +0200

    JWT ok

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     * 
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $encoder, SerializerInterface $serializer, EntityManagerInterface $em): Response
+    {
+        $data = $request->getContent();
+        $user = $serializer->deserialize($data, User::class, 'json');
+        $hash = $encoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($hash);
+        $em->persist($user);
+        $em->flush();
+        return $this->json([
+            'user' => $user->getEmail()
+        ]);
+    }
+}
